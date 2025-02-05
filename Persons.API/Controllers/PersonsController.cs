e301481c1daf2a1231c5c836d23067046d820182
Joel-Vides <joelvides00@gmail.com>
2025-02-04 20:02:34 -0600

    init

---
+++ Persons.API/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Persons.API.Controllers
+{
+    [ApiController]
+    [Route("api/persons")]
+    public class PersonsController : ControllerBase
+    {
+        public PersonsController()
+        {
+            
+        }
+    }
+}
